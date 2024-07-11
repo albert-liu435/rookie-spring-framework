5d3aa07028cd5498493df4134fc8f37bc051a50c
liuxili01 <liuxili@bestseller.com.cn>
2024-07-11 18:02:20 +0800

    Spring Bean定义与注册

---
+++ rookie-spring-framework-beans/src/main/java/com/rookie/bigdata/springframework/beans/factory/annotation/bean/MyBean.java
@@ -0,0 +1,22 @@
+package com.rookie.bigdata.springframework.beans.factory.annotation.bean;
+
+/**
+ * @Class MyBean
+ * @Description
+ * @Author rookie
+ * @Date 2024/7/11 11:39
+ * @Version 1.0
+ */
+public class MyBean {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
