9abb23014f3547d16961b2ccc512e771ec50c179
albert.liu435 <albert.liu435@gmail.com>
2024-09-02 22:48:53 +0800

    rookie-springboot-lifecycle

---
+++ rookie-springboot-web/rookie-springboot-lifecycle/src/main/java/com/rookie/bigdata/LifeCycleBootApplication.java
@@ -0,0 +1,21 @@
+package com.rookie.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname WebMvcBootApplication
+ * @Description
+ * @Author rookie
+ * @Date 2021/8/18 9:51
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class LifeCycleBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeCycleBootApplication.class,args);
+
+
+    }
+}
