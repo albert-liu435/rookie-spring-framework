4cd498f81c669cde37d068157057dfd4dca7dea9
liuxili01 <liuxili@bestseller.com.cn>
2024-12-10 18:02:28 +0800

    spring 启动流程

--- rookie-spring-web/rookie-spring-framework-web/src/test/java/com/rookie/bigdata/springframework/beans/factory/config/postprocessor/PostProcessorConfigTest.java
+++ rookie-spring-web/rookie-spring-framework-web/src/test/java/com/rookie/bigdata/springframework/beans/factory/config/postprocessor/PostProcessorConfigTest.java
@@ -1,6 +1,7 @@
 package com.rookie.bigdata.springframework.beans.factory.config.postprocessor;
 
 import com.rookie.bigdata.springframework.beans.factory.annotation.autowire.AutowiredConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
  * @Date 2024/9/10 9:22
  * @Version 1.0
  */
+@Slf4j
 class PostProcessorConfigTest {
 
 
@@ -19,6 +21,11 @@ class PostProcessorConfigTest {
     void test01(){
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(PostProcessorConfig.class);
+
+        User user=(User) context.getBean("buildUser");
+
+        log.error("获取user对象:{}",user.getName());
+
     }
 
 }
