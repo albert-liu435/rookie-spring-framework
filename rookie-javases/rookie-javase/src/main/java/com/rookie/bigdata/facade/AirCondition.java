d6c84507175fa5a6cd3ca75982768bf267e7dd6a
liuxili01 <liuxili@bestseller.com.cn>
2024-12-13 16:35:30 +0800

    java设计模式 门面设计模式

---
+++ rookie-javases/rookie-javase/src/main/java/com/rookie/bigdata/facade/AirCondition.java
@@ -0,0 +1,18 @@
+package com.rookie.bigdata.facade;
+
+/**
+ * @Class AirCondition
+ * @Description 控制类
+ * @Author rookie
+ * @Date 2024/12/13 16:32
+ * @Version 1.0
+ */
+public class AirCondition {
+    public void on() {
+        System.out.println("打开了空调....");
+    }
+
+    public void off() {
+        System.out.println("关闭了空调....");
+    }
+}
---
+++ rookie-javases/rookie-javase/src/main/java/com/rookie/bigdata/facade/TV.java
@@ -0,0 +1,18 @@
+package com.rookie.bigdata.facade;
+
+/**
+ * @Class TV
+ * @Description 电视类
+ * @Author rookie
+ * @Date 2024/12/13 16:32
+ * @Version 1.0
+ */
+public class TV {
+    public void on() {
+        System.out.println("打开了电视....");
+    }
+
+    public void off() {
+        System.out.println("关闭了电视....");
+    }
+}
