54ab860b3ad3633d306da1f4308ae85be577944f
liuxili01 <liuxili@bestseller.com.cn>
2024-12-13 13:35:02 +0800

    java设计模式 命令模式

---
+++ rookie-javases/rookie-javase/src/main/java/com/rookie/bigdata/command/co/Invoker.java
@@ -0,0 +1,20 @@
+package com.rookie.bigdata.command.co;
+
+/**
+ * @Class Invoker
+ * @Description 调用者
+ * @Author rookie
+ * @Date 2024/12/13 11:57
+ * @Version 1.0
+ */
+public class Invoker {
+    private Command command;
+
+    public void executeCommand() {
+        command.execute();
+    }
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+}
---
+++ rookie-javases/rookie-javase/src/main/java/com/rookie/bigdata/command/co/Receiver.java
@@ -0,0 +1,15 @@
+package com.rookie.bigdata.command.co;
+
+/**
+ * @Class Receiver
+ * @Description 接收者
+ * @Author rookie
+ * @Date 2024/12/13 11:56
+ * @Version 1.0
+ */
+public interface Receiver {
+    /**
+     * 接收者执行具体的命令内容
+     */
+    void action();
+}
