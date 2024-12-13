566acd660fec3b908bc86619d2d50e9dfa92bee4
liuxili01 <liuxili@bestseller.com.cn>
2024-12-13 09:08:06 +0800

    java设计模式 适配器模式

---
+++ rookie-javases/rookie-javase/src/main/java/com/rookie/bigdata/adapter/Duck.java
@@ -0,0 +1,13 @@
+package com.rookie.bigdata.adapter;
+
+/**
+ * @Class Duck
+ * @Description 鸭子(Duck)和火鸡(Turkey)拥有不同的叫声，Duck 的叫声调用 quack() 方法，而 Turkey 调用 gobble() 方法。
+ * @Author rookie
+ * @Date 2024/12/12 18:36
+ * @Version 1.0
+ */
+public interface Duck {
+
+    void quack();
+}
